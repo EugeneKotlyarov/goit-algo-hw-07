498fadeb7631157b87d165fb9cea5536df9b0101
EugeneKotlyarov <kotlyarov@ukrfast.com>
2025-01-28 21:45:01 +0200

    02 ready

---
+++ 02.py
@@ -0,0 +1,39 @@
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+def insert(root, key):
+    if root is None:
+        return TreeNode(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+
+def f_min(root):
+    if root is None:
+        return None
+    current = root
+    while current.left:
+        current = current.left
+    return current.val
+
+
+def main():
+    b = TreeNode(15)
+    b = insert(b, 20)
+    b = insert(b, 10)
+    b = insert(b, 30)
+    b = insert(b, 25)
+    b = insert(b, 35)
+
+    print("Мінімальне значення у дереві:", f_min(b))
+
+if __name__ == "__main__":
+    main()
