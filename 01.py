d1cc36c5251572d91f1e21c9b5bba6f8b58e3fc7
EugeneKotlyarov <kotlyarov@ukrfast.com>
2025-01-28 21:43:30 +0200

    01 ready

--- 01.py
+++ 01.py
@@ -1,2 +1,39 @@
+class TreeNode:
+    def __init__(self, key):
+        self.left = None
+        self.right = None
+        self.val = key
+
+
+def insert(root, key):
+    if root is None:
+        return TreeNode(key)
+    else:
+        if key < root.val:
+            root.left = insert(root.left, key)
+        else:
+            root.right = insert(root.right, key)
+    return root
+
+
+def f_max(root):
+    if root is None:
+        return None
+    current = root
+    while current.right:
+        current = current.right
+    return current.val
+
+
 def main():
-    pass
+    b = TreeNode(15)
+    b = insert(b, 20)
+    b = insert(b, 10)
+    b = insert(b, 30)
+    b = insert(b, 25)
+    b = insert(b, 35)
+
+    print("Максимальне значення у дереві:", f_max(b))
+
+if __name__ == "__main__":
+    main()
